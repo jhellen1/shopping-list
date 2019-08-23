1dae05e22411bec00b418b4519af20c0a877008f
Justin Hellen <jmhellen1@gmail.com>
2019-08-23 12:57:36 -0400

    added jQuery/js links to html, first 2 requirements of list in js file

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-$()
+function shoppingList() {
+    $('#js-shopping-list-form').submit(function(e){
+        e.preventDefault();
+        let newItem = $('#shopping-list-entry').val();
+        console.log(newItem);
+        let newProduct = `<li>
+        <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`;
+      $('.shopping-list').append(newProduct);
+      $('#shopping-list-entry').val('');
+    });
+    $('body').on('click','.shopping-item-toggle',function(e){
+        e.preventDefault();
+        $(e.target).closest('li').children('.shopping-item').toggleClass('shopping-item__checked')
+    })
+}
+
+$(shoppingList);
+
